9f13bfbef193194db077fcd1e0ab02fbf6b988dc
azusaMYK <11dctat@gmail.com>
2022-05-02 08:28:44 +0000

    validate and add column status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,7 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-<<<<<<< HEAD
         <title>Tasklist</title>
-=======
-        <title>MessageBoard</title>
->>>>>>> eaa546c1a0f563bf94e23c448e73b3266c8f6e76
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     </head>
@@ -15,6 +11,8 @@
         @include('commons.navbar')
 
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
 
